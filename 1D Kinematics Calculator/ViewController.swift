a95c50dc257ffadb86168c51cd7345840204017a
afriedsam <adf@thefriedsams.com>
2020-12-08 15:17:55 -0500

    Initial Commit

---
+++ 1D Kinematics Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1D Kinematics Calculator
+//
+//  Created by Aidan Friedsam on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 1D Kinematics CalculatorTests/_D_Kinematics_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  _D_Kinematics_CalculatorTests.swift
+//  1D Kinematics CalculatorTests
+//
+//  Created by Aidan Friedsam on 12/8/20.
+//
+
+import XCTest
+@testable import _D_Kinematics_Calculator
+
+class _D_Kinematics_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
